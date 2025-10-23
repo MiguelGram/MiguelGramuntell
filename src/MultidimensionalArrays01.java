a07c4b50667e6ca6541b33a505095a228f60a823
MiguelGramC <miggramar2@alu.edu.gva.es>
2025-10-23 17:53:27 +0000

    Initial commit

---
+++ src/MultidimensionalArrays01.java
@@ -0,0 +1,30 @@
+public class MultidimensionalArrays01 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        fill(num);
+
+    }
+
+    public static void fill(int num) {
+        String[][] nums = new String[num][num];
+
+        for(int fil = 0; fil<nums.length; fil++){
+            for(int col = 0; col<nums.length; col++){
+                if(col == fil){
+                    nums[fil][col] = "1";
+                }
+                else{
+                    nums[fil][col] = "0";
+                }
+            }
+        }
+        for(int fil = 0; fil<nums.length; fil++){
+            for(int col = 0; col<nums.length; col++){
+                System.out.print(nums[fil][col] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+}
